90c3e4adbf6fc1c37ac8a0cb11b129302d24bef7
Mateusz Kolasiński <57622258+Vesorad@users.noreply.github.com>
2022-07-18 16:26:12 +0200

    Prototype drag and drop

---
+++ Assets/Scripts/UI/DragDropItemFacade.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDropItemFacade : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    [SerializeField] private RectTransform rectTransform = null;
+    [SerializeField] private CanvasGroup canvasGroup = null;
+    [SerializeField, Min(0)] private float alphaImage = 0;
+
+    private Vector2 startPosition;
+
+    private void Awake() => startPosition = rectTransform.anchoredPosition;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("OnPoint");
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        canvasGroup.alpha = 0.6f;
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData) => rectTransform.anchoredPosition += eventData.delta;
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition = startPosition;
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+    }
+}
