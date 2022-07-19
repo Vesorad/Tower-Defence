cf49ab7ea58a22ad2c8a3883da612dbf4f5e3196
Mateusz Kolasiński <57622258+Vesorad@users.noreply.github.com>
2022-07-19 20:54:27 +0200

    Add start menu

---
+++ Assets/Scripts/UI/MainManuController.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.UI
+{
+    public class MainManuController : MonoBehaviour
+    {
+        private const int GAME_SCENE_INDEX = 1;
+
+        [SerializeField, Min(0.1f)] private float lifeTimeStartIcon = 0.1f;
+        [SerializeField] private GameObject creatorsCanvas = null;
+
+        private float timeToDisableStartIcon;
+
+        private void Start()
+        {
+            timeToDisableStartIcon = Time.time + lifeTimeStartIcon;
+        }
+
+        private void Update()
+        {
+            if (creatorsCanvas.activeSelf && Time.time >= timeToDisableStartIcon)
+                creatorsCanvas.SetActive(false);
+        }
+
+
+        public void OnClickPlay() => SceneManager.LoadScene(GAME_SCENE_INDEX);
+
+        public void OnClickSettings()
+        {
+            //TODO
+        }
+
+        public void OnClickCredits()
+        {
+            //TODO
+        }
+
+        public void OnClickScoreboard()
+        {
+            //TODO
+        }
+    }
+}
